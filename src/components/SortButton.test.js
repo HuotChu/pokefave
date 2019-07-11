4e0be542b6101030ca81806f58f37f16d6fb9342
scottbishop <scottb@aggdata.com>
2019-07-11 02:56:22 -0400

    removed extra import

--- src/components/SortButton.test.js
+++ src/components/SortButton.test.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import ListContext from '../contexts/list-context';
